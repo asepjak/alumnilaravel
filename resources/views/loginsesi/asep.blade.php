b3c366f7bec07e1998dbc6f2f192b52ec9bda262
Asep <septianprogaming@gmail.com>
2024-10-04 16:54:32 +0700

    login lebeh bagos

--- resources/views/loginsesi/asep.blade.php
+++ resources/views/loginsesi/asep.blade.php
@@ -1,27 +1,70 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AlumniLink</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-<div class="w-50 center border rounded px-3 py-3 mx-auto">
-    <h1>Login</h1>
-    <form action="/alumni/login" method="post">
-        @csrf
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" name="email" class="form-control">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">password</label>
-            <input type="password" name="password" class="form-control">
-        </div>
-        <div class="mb-3 d-grid">
-            <button name="submit" type="submit" class="btn btn-primary">Login</button>
-        </div>
-    </form>
-</div>
+    <div class="login-container">
+        <h1>Login</h1>
+        <form action="/alumni/login" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password" class="form-label">Password</label>
+                <div class="password-input-container">
+                    <input type="password" name="password" id="password" class="form-control">
+                    <button type="button" id="togglePassword" class="password-toggle">
+                    </button>
+                </div>
+            </div>
+            <button name="submit" type="submit" class="btn-primary">Login</button>
+        </form>
+    </div>
+    <script src="{{ asset('js/login.js') }}"></script>
 </body>
+</html> -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Login Page</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="login-box">
+        <h2>Login</h2>
+        <form action="" method="post">
+          <div class="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" id="password" required="" />
+            <label>Password</label>
+            <span class="password-toggle-icon"></span>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
+    </div>
+    <script src="{{ asset('js/login.js') }}"></script>
+  </body>
 </html>
